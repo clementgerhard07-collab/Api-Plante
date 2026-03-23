e7cb557554e6b5f32169267ae67e42a418744199
clementgerhard07-collab <clementgerhard07@gmail.com>
2026-03-23 09:06:48 +0100

    Delete Projet NSI - Plante/djangotutorial directory

--- Projet NSI - Plante/djangotutorial/polls/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.upload, name="upload"),
-    path("identify/", views.analyse_plante, name="identify"),
-    path('', views.analyse_plante, name='analyse_plante'),
-]
